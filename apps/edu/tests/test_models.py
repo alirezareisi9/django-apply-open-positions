8595145f65fca710034f489ce0799da3e2947239
alirezareisi9 <alirezareisi.sr@gmail.com>
2025-10-06 17:29:40 +0330

    Fix/ Fix and Complete test_models, because of changing db schema: need to change testcases

--- apps/edu/tests/test_models.py
+++ apps/edu/tests/test_models.py
@@ -14,6 +14,9 @@ from apps.edu.models import (
     UniversityImage,
     Position,
     DegreeChoices,
+    Authorship,
+    Teaching,
+    UniversityCourse,
 )
 
 
@@ -25,8 +28,10 @@ class ModelsTests(TestCase):
         cls.file = SimpleUploadedFile("file.txt", cls.content, "text/plain")
 
         cls.major = Major.objects.create(title="major", degree="BACHELOR")
+        
         cls.field = Field.objects.create(title="field")
         cls.field.majors.add(cls.major)
+        
         cls.professor = Professor.objects.create(
             first_name="professor",
             last_name="professor",
@@ -36,8 +41,11 @@ class ModelsTests(TestCase):
             photo=cls.photo,
         )
         cls.professor.fields.add(cls.field)
+        
         cls.publication = Publication.objects.create(title="test", file=cls.file)
-        cls.publication.authors.add(cls.professor)
+        # cls.publication.authors.add(cls.professor)
+        Authorship.objects.create(professor=cls.professor, publication=cls.publication)
+
         cls.course = Course.objects.create(
             title="test",
             major=cls.major,
@@ -45,17 +53,22 @@ class ModelsTests(TestCase):
             price=2000,
             is_accessible=True,
         )
-        cls.course.professors.add(cls.professor)
+        # cls.course.professors.add(cls.professor)
+        Teaching.objects.create(professor=cls.professor, course=cls.course)
+
         cls.university = University.objects.create(
             name="test",
             logo=cls.photo,
         )
-        cls.university.professors.add(cls.professor)
-        cls.university.courses.add(cls.course)
+        # cls.university.professors.add(cls.professor)
+        # cls.university.courses.add(cls.course)
+        UniversityCourse.objects.create(university=cls.university, course=cls.course)
         cls.university.majors.add(cls.major)
+
         cls.university_image = UniversityImage.objects.create(
             image=cls.photo, university=cls.university
         )
+
         cls.position = Position.objects.create(
             title="test",
             description="test here",
@@ -64,12 +77,16 @@ class ModelsTests(TestCase):
             major=cls.major,
             deadline=datetime.date.today(),
         )
+
+        cls.professor.university = cls.university
+
         cls.education = Education.objects.create(
             professor=cls.professor,
             major=cls.major,
             university=cls.university,
             graduation_year=2000,
         )
+
 
     def test_major(self):
         major = self.major
@@ -93,6 +110,7 @@ class ModelsTests(TestCase):
         professor.photo.close()
         self.assertEqual(len(professor.fields.all()), 1)
         self.assertIn(self.field, professor.fields.all())
+        self.assertEqual(professor.university, self.university)
 
     def test_publication(self):
         publication = self.publication
@@ -119,8 +137,6 @@ class ModelsTests(TestCase):
         university.logo.open()
         self.assertEqual(university.logo.read(), self.content)
         university.logo.close()
-        self.assertEqual(len(university.professors.all()), 1)
-        self.assertIn(self.professor, university.professors.all())
         self.assertEqual(len(university.courses.all()), 1)
         self.assertIn(self.course, university.courses.all())
         self.assertEqual(len(university.majors.all()), 1)
