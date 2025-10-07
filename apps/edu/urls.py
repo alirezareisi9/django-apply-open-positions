4a1d56d9a64fb0e63696a67c5fd8e93386db2dfc
alirezareisi9 <alirezareisi.sr@gmail.com>
2025-10-07 10:58:22 +0330

    Refactor/ Delete urls and Comment views of edu app

--- apps/edu/urls.py
+++ apps/edu/urls.py
@@ -6,9 +6,9 @@ from django.conf.urls.static import static
 
 
 app_name = "edu"
-urlpatterns = [
-    path("", views.home, name="home"),
-    path("admin", views.fakeadmin, name="gottem"),
-    path("detail/<int:id>/", views.uni_detail, name="detail"),
-    path("profile/<int:id>/", views.prof_detail, name="profile"),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns = []
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.STATIC_URL, document_root=settings.STATIC_ROOT
+    ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
