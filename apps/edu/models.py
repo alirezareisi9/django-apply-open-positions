243a0152bc211a6de4e19442512fbdc6a6df49d2
alirezareisi9 <alirezareisi.sr@gmail.com>
2025-10-10 22:53:06 +0330

    Feat/ Append views and templates of edu - Delete projects field in Professor model class - Define views and templates of edu + Fix bugs of views and templates of edu

--- apps/edu/models.py
+++ apps/edu/models.py
@@ -47,7 +47,6 @@ class Professor(models.Model):
     )
     department = models.CharField(max_length=100, blank=True, null=True)
     awards_and_honors = models.TextField(blank=True, null=True)
-    projects = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.first_name + " " + self.last_name
--- apps/edu/views.py
+++ apps/edu/views.py
@@ -3,9 +3,7 @@ from . import models
 from django.contrib.auth.models import User
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-
-
-from .filters import *
+from . import filters
 
 
 class PositionListView(ListView):
@@ -31,6 +29,19 @@ class UniversityListView(ListView):
         return self.model.objects.all()
 
 
+class UniversityDetailView(DetailView):
+    model = models.University
+    context_object_name = "university"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context['courses'] = self.object.courses.all()
+        context['majors'] = self.object.majors.all()
+
+        return context
+
+
 class ProfessorListView(ListView):
     model = models.Professor
     context_object_name = "professors"
@@ -40,6 +51,18 @@ class ProfessorListView(ListView):
         return self.model.objects.all()
 
 
+class ProfessorDetailView(DetailView):
+    model = models.Professor
+    context_object_name = "professor"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context['fields'] = self.object.fields.all()
+
+        return context
+
+
 class PublicationListView(ListView):
     model = models.Publication
     context_object_name = "publications"
@@ -47,7 +70,25 @@ class PublicationListView(ListView):
 
     def get_queryset(self):
         return self.model.objects.all()
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
 
+        context['authors'] = self.object.authors.all()
+
+        return context
+
+
+class PublicationDetailView(DetailView):
+    model = models.Publication
+    context_object_name = "publication"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context['authors'] = self.object.authors.all()
+
+        return context
 
 class CourseListView(ListView):
     model = models.Course
@@ -56,3 +97,22 @@ class CourseListView(ListView):
 
     def get_queryset(self):
         return self.model.objects.all()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context['professors'] = self.object.professors.all()
+
+        return context
+
+
+class CourseDetailView(DetailView):
+    model = models.Course
+    context_object_name = "course"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context['professors'] = self.object.professors.all()
+
+        return context
